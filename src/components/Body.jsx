7ea1a350efcfd0e719466612d3411fdee15df851
CASanchezc5 <cascqf@gmail.com>
2024-09-12 10:12:48 -0500

    Se modifico el section en el index.css

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,17 +1,21 @@
-import "../index.css"
-import Header from "./Header"
-import Main from "./Main"
-import SectionMain from "./SectionMain"
+import "../index.css";
+import Header from "./Header";
+import Main from "./Main";
+import SectionMain from "./SectionMain";
 
 const Body = () => {
   return (
     <div className="body">
       <Header />
       <Main />
-      <SectionMain className="sectionMain1"/>
-      <SectionMain className="sectionMain2"/>
+      <div className="sectionMain1">
+        <SectionMain  />
+      </div>
+      <div className="sectionMain2">
+        <SectionMain  />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
